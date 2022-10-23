9099de3d47a35fd290ed9bc6dd0b3065b58955da
dyamada-jaki <edy.jaki@gmail.com>
2022-10-24 00:15:30 +0900

    v0.6.0 への対応

--- src/main/java/io/github/kuberatest/actiongenerate/ActionGeneratorTool.java
+++ src/main/java/io/github/kuberatest/actiongenerate/ActionGeneratorTool.java
@@ -3,6 +3,7 @@ package io.github.kuberatest.actiongenerate;
 public class ActionGeneratorTool {
     public static void main(String[] args) {
         ActionGenerator actionGenerator = new ActionGenerator();
+        actionGenerator.setTestcaseExcelFilePath(args[1]);
         actionGenerator.execute(args[0]);
     }
 }
